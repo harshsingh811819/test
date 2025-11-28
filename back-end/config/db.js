7fed2ff4143de92f7da5a92ccdde099d7b8e8c51
harshsingh811819 <137808968+harshsingh811819@users.noreply.github.com>
2025-11-28 08:15:46 +0530

    Delete back-end directory

--- back-end/config/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-export const dbConnect = async() => {
-   const conn = await mongoose.connect('mongodb://127.0.0.1:27017/Quires');
-   if(conn){
-    console.log("DB connected successfully.....");
-   }
-}
